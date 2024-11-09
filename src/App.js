57c306bf6d0537fdb610d4f01671121a82bf69de
Abdullah <aa1672422@gmail.com>
2024-11-09 16:08:01 +0530

    EMI Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [principale, setPrincipale] = useState(0);
+  const [interest, setInterest] = useState(0);
+  const [years, setYears] = useState(0);
+  const [emi, setEMI] = useState(0);
+
+  const handleChange = (e) => {
+    console.log(e.target.id, e.target.value);
+    const id = e.target.id;
+    const value = parseInt(e.target.value);
+    if (id === 'principale') {
+      setPrincipale(value);
+    } else if (id === 'interest') {
+      setInterest(value);
+    } else {
+      setYears(value);
+    }
+  }
+
+  // P(r(1+r)^n/((1+r)^n)-1))
+  const calculateEMI = () => {
+    let r = interest;
+    if (principale && r && years) {
+      r = r / 12 / 100; // per month
+      const calcPow = Math.pow(1 + r, years * 12);
+      const amount = principale * ((r * calcPow) / (calcPow - 1));
+      setEMI(Math.round(amount));
+    }
+  }
+
+  useEffect(() => {
+    calculateEMI();
+  }, [principale, interest, years])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='loan-calc'>
+      <h1>Mortgage Caclulator</h1>
+
+      <div className='inputes'>
+        <p>Principale:</p>
+        <input
+          onChange={handleChange}
+          type='number' id='principale' />
+
+        <p>Interest:</p>
+        <input
+          onChange={handleChange}
+          type='number' id='interest' />
+
+        <p>Years:</p>
+        <input
+          onChange={handleChange}
+          type='number' id='year' />
+      </div>
+
+      <div className='output'>
+        Your EMI is {emi}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
